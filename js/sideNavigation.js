dae6a6c41e5421b5475ae0efeb3155ee2b9550d0
unknown <jggianakopoulos@gmail.com>
2019-11-16 15:43:11 -0500

    add reset sheet, folders, start look

---
+++ js/sideNavigation.js
@@ -0,0 +1,10 @@
+function closeNav() {
+	var x = document.getElementsByClassName("sideNav");
+	x[0].style.width = "0px";
+}
+
+function openNav() {
+	var x = document.getElementsByClassName("sideNav");
+	x[0].style.width = "150px";
+}
+
